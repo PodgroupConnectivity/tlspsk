9788b9e05ce922509065521ea0df30d4dc250ed0
kostiantyn.chertov <kostiantyn.chertov@podgroup.com>
2021-05-19 17:05:00 +0300

    merge with version key & API v1

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=4
+###
+#
+# Copyright (c) 2020-2021 Pod Group Ltd
+# Authors : Kostiantyn Chertov <kostiantyn.chertov@podgroup.com>
+
+from setuptools import setup
+
+REQUIREMENTS = open('requirements.txt', 'rt').read().splitlines()
+LONG_DESCRIPTION = open('README.md', 'rt').read()
+
+setup(name='tlspsk-client',
+      version='0.1.0',
+      description='TLSv1.3 client fork configured as PSK fork from tls1.3 client',
+      long_description=LONG_DESCRIPTION,
+      author='Kostiantyn Chertov',
+      author_email='kostiantyn.chertov@podgroup.com',
+      url='https://github.com/PodgroupConnectivity/tlspsk-client',
+      packages=['tlspsk'],
+      platforms=['win32', 'linux2'],
+      license='MIT License',
+      install_requires=REQUIREMENTS
+)
--- test_tls.py
+++ test_tls.py
@@ -1,20 +1,17 @@
+from os import sep
 import socket
 import time
-from tls import TLSClientSession
+from tlspsk import TLSClientSession
 from datetime import datetime, timedelta, timezone
 
+from credentials import psk, iccid
 
 def main():
 
     quit = False
     sock = None
 
-    # server = '3.248.203.71'
-    # port = 6000
-    # server = '3.249.47.6'   34.253.244.76
-    # server = '34.253.244.76'
-    # server = '63.34.4.88'
-    server = '34.253.244.76'  #'52.213.234.222'
+    server = '34.253.244.76'
     port = 11111
     # server = "127.0.0.1"
     # port = 443
@@ -25,19 +22,9 @@ def main():
         if data == b"bye\n":
             quit = True
 
-    psk = bytes.fromhex(
-        '5a53547a59645b585b45405e5a727069'
-    )
-    iccid = bytes.fromhex(
-        '984405529081369836f5'
-    )
-    # session = TLSClientSession(
-    #     server_names="127.0.0.1", psk=psk, data_callback=callback, psk_only=True, early_data=b"hoho"
-    # )
     session = TLSClientSession(
         server_names=server, psk=psk, psk_label=iccid, data_callback=callback, psk_only=True
     )
-    # session = TLSClientSession(server_names="127.0.0.1", data_callback=callback)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server, port))
@@ -61,31 +48,13 @@ def main():
 
     dt = datetime.utcnow()
     stime = dt.strftime('%y/%m/%d %H:%M:%S UTC')
-    # data = bytes('POST /simmap/tp/ HTTP/1.1\x0d\x0aHost: localhost\x0d\x0aContent-Length: 138\x0d\x0a\x0d\x0a' +
-    #              '{"iccid": "984405529081369836f5", ' +
-    #              '"imei": "3a25091040261803", ' +
-    #              '"tp": "test-from-python:' + stime + '"}', 'utf-8')
-    # data = bytes('POST /simmap/tp/ HTTP/1.1\x0d\x0a' +
-    #              'Content-Type: application/json\x0d\x0a' +
-    #              'Connection: close\x0d\x0a' +
-    #              'User-Agent: PodSender/0.2\x0d\x0a' +
-    #              'Host: ' + server + '\x0d\x0a' +
-    #              'Content-Length: 126\x0d\x0a\x0d\x0a' +
-    #              '{"iccid":"984405529081369836f5",' +
-    #              '"imei":"3a75250047633502",' +
-    #              '"tp":"ffffffff7f9d00ffbf03021fe200000083eb0000001c4800100000000008"}', 'utf-8')
 
     # iot pushing telemetry data with POST
     data_string = '{"temperature": 24.15}'
-    # data_string = '{"iccid":"984405529081369836f5",' + \
-    #               '"imei":"3a75250047633502",' + \
-    #               '"tp":"ffffffff7f9d00ffbf03021fe200000083eb0000001c4800100000000008"}'
-
-    # data = bytes('POST /v1/data/51523143572089723526?iccid=984405529081369836f5 HTTP/1.1\x0d\x0a' +
     data = bytes('POST /v1/data/51523143572089723527?iccid={0} HTTP/1.1\x0d\x0a'.format(iccid.hex()) +
-    # data = bytes('POST /v1/tp/ HTTP/1.1\x0d\x0a' +
                  'Host: pod.iot.platform\x0d\x0a' +
                  'Content-Length: {0}\x0d\x0a\x0d\x0a{1}'.format(len(data_string), data_string), 'utf-8')
+
     # iot getting config data with GET
     # data = bytes('GET /v1/config/51523143572089723526?iccid=984405529081369836f5 HTTP/1.1\x0d\x0a' +
     #              '\x0d\x0a', 'utf-8')
@@ -94,12 +63,12 @@ def main():
     print('app_data: {0}'.format(app_data.hex()))
 
     sock.sendall(app_data)
-    time.sleep(5)
+    time.sleep(1)
     resp = sock.recv(4096)
     print('resp: {0}'.format(resp.hex()))
     parser.send(resp)
 
-    time.sleep(5)
+    time.sleep(0.5)
     resp = sock.recv(4096)
     print('resp: {0}'.format(resp.hex()))
     parser.send(resp)
@@ -107,23 +76,6 @@ def main():
     sock.sendall(session.pack_close())
     sock.close()
     print('done!')
-
-    # quit = False
-    # session = session.resumption(data_callback=callback)
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.connect((server, port))
-    # sock.sendall(session.pack_client_hello())
-    #
-    # parser = session.parser()
-    # while not quit:
-    #     server_data = sock.recv(4096)
-    #     parser.send(server_data)
-    #     data = parser.read()
-    #     if data:
-    #         sock.sendall(data)
-    #
-    # sock.sendall(session.pack_close())
-    # sock.close()
 
 
 main()
